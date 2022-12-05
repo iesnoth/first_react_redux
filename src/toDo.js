678c91664333ce5c722f04eb23a0184f7bc09342
Victoria Beasley <iesnoth91@gmail.com>
2022-12-04 23:31:19 -0600

    annotated stuff

--- src/toDo.js
+++ src/toDo.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { addTodo, removeOne, clearAll } from './features/toDoSlice'
+import { addTodo, clearAll, removeOne } from './features/toDoSlice'
 
 function ToDo() {
+    //the list of items that are the state, pulled from the todo slice
     const items = useSelector((state) => state.todos.items)
     const dispatch = useDispatch()
+    //for the onClick event in removeOne
     const [input, setInput] = useState('')
-
-    const renderItems = items.map((item, index) => <li key={index} onClick={() => dispatch(removeOne(index))}>{item}</li>)
-
-    const submitForm = (e) => {
-        e.preventDefault()
+    //maps over the list of items and, when one is clicked on, sends in the removeOne reducer to remove the item
+    //based on the assigned index (i)
+    const renderItems = items.map((item, i) => <li key={i} onClick={() => dispatch(removeOne(i))}>{item}</li>)
+    //adds a new item based on the user input in the text field
+    const submitForm = (newInput) => {
+        newInput.preventDefault()
         dispatch(addTodo(input))
     }
 
-    return (
+    return(
         <div>
-            <form onSubmit={(e) => submitForm(e)}>
-                <input type="text" onChange={(e) => setInput(e.target.value)} />
-                <button type="submit">Submit</button>
+            {/* tells the form to launch the addToDo reducer upon the submit button being fired */}
+            <form onSubmit={(newInput)=>submitForm(newInput)}>
+                <input type="text" onChange={(e)=>setInput(e.target.value)}/>
+                <button type="submit">Submit to do Item</button>
             </form>
             <ul>
                 {renderItems}
             </ul>
-            <button onClick={() => dispatch(clearAll())}>Clear</button>
+            <button onClick={()=> dispatch(clearAll())}>Clear All</button>
         </div>
     )
+
 }
 
 export default ToDo
